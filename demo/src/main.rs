c71a975fa2595babb08475ea8968201a48aefe87
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2025-12-03 19:45:19 +0100

    Demo with rug

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,17 @@
+use rug::Integer;
+
+use flint_lll_sys::*;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let mut x = Integer::from(42);
+
+        let mut f: fmpz_t = [0];
+        fmpz_init(&mut f);
+        fmpz_set_si(&mut f, 43);
+
+        fmpz_get_mpz(x.as_raw_mut(), &f);
+
+        fmpz_clear(&mut f);
+    }
 }
